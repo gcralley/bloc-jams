b654992ac030976d527056c9428b3610111cd20f
gcralley <gcralley@gmail.com>
2017-04-13 14:24:34 -0500

    Mess around with error code

--- scripts/album.js
+++ scripts/album.js
@@ -13,7 +13,7 @@
      ]
  };
  
- // Another Example Album
+ 
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -57,23 +57,21 @@ var albumLorca = {
  };
 
  var setCurrentAlbum = function(album) {
-     // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+ 
      albumSongList.innerHTML = '';
  
-     // #4
+ 
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -86,9 +84,9 @@ var albumLorca = {
 var currentAlbum = setCurrentAlbum();
 
 function changeAlbum(currentAlbum) = {
-    if (currentAlbum == "albumPicasso") {
+    if (currentAlbum === albumPicasso) {
     setCurrentAlbum(albumMarconi);    
-    } else if (currentAlbum == "albumMarconi") {
+    } else if (currentAlbum === albumMarconi) {
     setCurrentAlbum(albumLorca);
 } else {
     setCurrentAlbum(albumPicasso);
