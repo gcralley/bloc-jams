caa4406aee7366bc5f24dda391b8ed85b5979f54
gcralley <gcralley@gmail.com>
2017-04-13 13:52:06 -0500

    Add JS function to toggle between albums

--- scripts/album.js
+++ scripts/album.js
@@ -1,18 +1,19 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21' },
-        { title: 'Magenta', duration: '2:15' }
-    ]
-};
-
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ // Another Example Album
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -28,6 +29,21 @@ var albumPicasso = {
      ]
  };
 
+var albumLorca = {
+    title: 'The Aurora',
+    artist: 'Federico Garcia Lorca',
+    label: 'El Surrealismo',
+    year: '1914',
+    albumArtUrl: 'assets/images/album_covers/lorca.jpg',
+    songs: [
+        { title: 'Bodas de Sangre', duration: '2:30' },
+        { title: 'Poeta en Nueva York', duration: '3:01' },
+        { title: 'No me digas eso', duration: '1:30' },
+        { title: 'Fin de semana', duration: '7:22' },
+        { title: 'La muerte, sin ti', duration: '4:15' }
+    ]
+};
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,25 +56,43 @@ var albumPicasso = {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    
-    albumSongList.innerHTML = '';
-    
-    for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
+ var setCurrentAlbum = function(album) {
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
+
+var currentAlbum = setCurrentAlbum();
+
+function changeAlbum(currentAlbum) = {
+    if (currentAlbum == "albumPicasso") {
+    setCurrentAlbum(albumMarconi);    
+    } else if (currentAlbum == "albumMarconi") {
+    setCurrentAlbum(albumLorca);
+} else {
+    setCurrentAlbum(albumPicasso);
+}
 };
 
-window.onload = function() {
-    setCurrentAlbum(albumPicasso);
-};
+currentAlbum.addEventListener("click", changeAlbum);
