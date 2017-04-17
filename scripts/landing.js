f66956312fc2dae13103a6c7157fb7ce82b6a496
gcralley <gcralley@gmail.com>
2017-04-17 13:45:39 -0500

    Change window event scope

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,23 @@
-var pointsArray = document.getElementsByClassName('point'); 
-
 var animatePoints = function() {
-         var revealPoint = function() {
-            $(this).css({
-             opacity: 1,
-             transform: 'scaleX(1) translateY(0)'
-         });
-    };
-
-    $.each($('.point'), revealPoint);
+	var revealPoint = function() {
+  	$(this).css({
+    opacity: 1,
+    transform: 'scaleX(1) translate(Y)'
+    });
+  };
+   $.each($('.point'), revealPoint);
 };
 
 $(window).load(function() {
      if ($(window).height() > 950) {
          animatePoints();
     }
-    });
-               
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+ var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  $(window).scroll(function(event) {
+     $(window).scroll(function(event) {
          if ($(window).scrollTop() >= scrollDistance) {
              animatePoints();
-       } else if (pointsArray[0].getBoundingClientRect().top <= 500) {
-            animatePoints(pointsArray);
         }
     });
-
+});
